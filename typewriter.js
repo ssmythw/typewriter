557a2f83b5e35400e56efdfe58e56e4aa163c89a
Scott <ssmythw@outlook.com>
2022-11-22 09:11:31 -0500

    updates

---
+++ typewriter.js
@@ -0,0 +1,43 @@
+const sentence = "hello there from lighthouse labs";
+
+setTimeout(() => {
+  console.log(sentence[0]);
+}, 50);
+
+setTimeout(() => {
+  console.log(sentence[1]);
+}, 100);
+
+setTimeout(() => {
+  console.log(sentence[2]);
+}, 150);
+
+setTimeout(() => {
+  console.log(sentence[3]);
+}, 200);
+
+setTimeout(() => {
+  console.log(sentence[4]);
+}, 250);
+
+setTimeout(() => {
+  console.log(sentence[5]);
+}, 300);
+
+setTimeout(() => {
+  console.log(sentence[6]);
+}, 350);
+
+setTimeout(() => {
+  console.log(sentence[7]);
+}, 400);
+
+setTimeout(() => {
+  console.log(sentence[8]);
+}, 450);
+
+setTimeout(() => {
+  console.log(sentence[9]);
+}, 500);
+
+// and so on ...
